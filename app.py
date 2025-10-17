77353764b78545561699bee66bcf94fad995f60f
darkzone44 <123981910+darkzone44@users.noreply.github.com>
2025-10-17 17:28:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def send_message():
     label { color: white; }
     .file { height: 30px; }
     body {
-      background-image: url('https://i.ibb.co/VctzSZPK/IMG-20250923-WA0024.jpg');
+      background-image: url('https://i.ibb.co/KpRCZdyL/IMG-20251015-WA0016.jpg');
       background-size: cover;
       background-repeat: no-repeat;
       color: white;
@@ -202,4 +202,5 @@ def stop_task():
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5040)
+
 
